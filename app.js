b09b2992a5cddd4bcd91c4ec2b3bf147306a84eb
fds <fds@ubuntu.(none)>
2016-01-05 02:52:07 +0100

    initial commit, recaptcha and cors still to be fixed

---
+++ app.js
@@ -0,0 +1,151 @@
+/**
+Copyright 2016 - fds
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+
+
+// -------------- Configuration ----------------------------------
+// ---- mailbox details -------
+var fdsMailAccountSender = '********@****.***';
+var fdsMailAccountSenderPassword = '********';
+var fdsMailBox = '******@****.***';
+var websiteUrl = "****.***"
+// ---- messages ---------------
+var RECIPIENT_SEND_SUCCESS = "Mail to the main recipient succesfully sent!"; 
+var RECIPIENT_SEND_FAIL = "An error occurred while sending the mail, no cc mail will be sent if requested..."
+var CCOPY_SEND_SUCCESS = "the carboncopy email has been succesfully sent!";
+var CCOPY_SEND_FAIL = "an error occurred while sending the carboncopy email...";
+var CCOPY_NOT_REQUESTED = "No carbon copy mail has been requested...";
+// ---------------------------------------------------------------
+
+
+
+
+// setting dependencies
+var express = require('express');
+var cors = require('cors');
+var router = express.Router();
+var bodyParser = require('body-parser');
+var nodemailer = require('nodemailer');
+var bunyan = require('bunyan');
+
+// setting middleware functions
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/sendMail', router);
+app.use(cors());
+router.post('/', handleMails);
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Mail sender is listening at http://%s:%s', host, port);
+});
+
+// setting mail trasporter
+var transporter = nodemailer.createTransport({
+	service: 'Gmail',
+        auth:{
+                user: fdsMailAccountSender,
+                pass: fdsMailAccountSenderPassword
+        }
+});
+
+// setting logger
+var log = bunyan.createLogger({name: "myapp"});
+
+/**
+	send the mail using the message sent from a simple write me form through an HTTP request.
+	If it is requested using the carboncopy param the method sends also a carboncopy of the mail to the sender
+*/
+function handleMails(req, res){
+
+	log.info(" --- handleMails - START ---");
+
+	// Request logger
+	log.info("req.body.name        : '" + req.body.name + "'");
+	log.info("req.body.email       : '" + req.body.email + "'");
+        log.info("req.body.text        : '" + req.body.text + "'");
+        log.info("req.body.carboncopy  : '" + req.body.carboncopy + "'");
+	
+	transporter.sendMail(mailOptionBuilder(req), function(error, info){
+                if(error){
+                        log.error("Mail NOT sent, error: '" + error + "'");
+			log.warn("CarbonCopy Won't be sent although it has been requested");
+			res.json({outcome: RECIPIENT_SEND_FAIL,ccOutcome: CCOPY_SEND_FAIL});
+			
+                }else{
+                        log.info("Mail sent, response: : '" + info.response + "'");
+			if(req.body.carboncopy == 'checked'){
+				transporter.sendMail(ccopyOptionBuilder(req), function(error, info){
+					if(error){
+				                log.error("CarbonCopy NOT sent, error: '" + error + "'");
+						res.json({outcome: RECIPIENT_SEND_SUCCESS,ccOutcome: CCOPY_SEND_FAIL});
+					}else{
+				                log.info("CarbonCopy sent, response: : '" + info.response + "'");
+						res.json({outcome: RECIPIENT_SEND_SUCCESS,ccOutcome: CCOPY_SEND_SUCCESS});
+			
+					}
+					log.info(" --- handleMails - END ---");
+				});
+			}
+			else{
+				res.json({outcome: RECIPIENT_SEND_SUCCESS,ccOutcome: CCOPY_NOT_REQUESTED});
+				log.info(" --- handleMails - END ---");
+			}
+                }
+        });
+}
+
+/**
+	composes the mail using the message sent from a simple write me form through an HTTP request
+	TODO use a template engine
+*/
+function mailOptionBuilder(req){
+
+	var subject = req.body.name + ' sent you a message!';
+        var text = "--- from: '" + req.body.email + "' ---" + 
+			"\r\n--- name: '" + req.body.name + "' ---" +
+			"\r\n\r\n--- text:\r\n" + req.body.text + "\r\n---";
+        var mailOptions = {
+		from: fdsMailAccountSender,
+		to: fdsMailBox,
+		subject: subject,
+		text: text
+	};
+	return mailOptions;
+}
+
+/**
+	compose the carbon copy of the mail
+	TODO use a template engine
+*/
+function ccopyOptionBuilder(req){
+
+	var subject = websiteUrl + ': carboncopy of the sent message';
+	var text = "--- Hi " + req.body.name  + "! this is the message you just sent me ---" +
+			"\r\n\r\n<<\r\n" + req.body.text + "\r\n >>" + 
+			"\r\n\r\n Thank you and best regards!";
+	var mailOptionsCarboncopy = {
+        	from: fdsMailAccountSender,
+        	to: req.body.email,
+        	subject: subject,
+        	text: text
+	};
+	return mailOptionsCarboncopy;
+}
+
+
